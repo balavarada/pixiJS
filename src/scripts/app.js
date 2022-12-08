18eae5a6e640c5ca8d7563ba364aa9e591a82413
Varadarajan <Balakrishnan.Varadarajan@sgdigital.com>
2022-12-08 15:42:49 +0530

    Canvas resixe property

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,7 +2,8 @@ import * as PIXI from "pixi.js";
 
 export class App {
     run() {
-        this.app = new PIXI.Application();
+        this.app = new PIXI.Application({resizeTo: window});
         console.log(this.app);
+        document.body.appendChild(this.app.view);
     }
 }
