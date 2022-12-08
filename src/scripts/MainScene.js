de459f28b10ca99e8c8b9259ee341469f09d7905
Varadarajan <Balakrishnan.Varadarajan@sgdigital.com>
2022-12-08 22:16:09 +0530

    Creating Puzzle Pieces.

--- src/scripts/MainScene.js
+++ src/scripts/MainScene.js
@@ -1,11 +1,12 @@
 import * as PIXI from "pixi.js";
-
 import { Globals } from "./Globals";
+import { PuzzleGrid } from "./PuzzleGrid";
 
 export class MainScene {
     constructor() {
         this.container = new PIXI.Container;
         this.createBckground();
+        this.createPuzzleGrid();
     }
 
     createBckground() {
@@ -21,5 +22,12 @@ export class MainScene {
         this.bg.width = window.innerWidth;
         this.bg.height = window.innerHeight;
         this.container.addChild(this.bg);
+        
+    }
+
+    createPuzzleGrid() {
+        const grid = new PuzzleGrid();
+        this.container.addChild(grid.container);
+
     }
 }
