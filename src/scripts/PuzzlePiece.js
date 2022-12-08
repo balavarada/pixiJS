9f777388dfa138e72c8ad6c4968c992a657f13dc
Varadarajan <Balakrishnan.Varadarajan@sgdigital.com>
2022-12-08 23:04:06 +0530

    Adding interactivity.

--- src/scripts/PuzzlePiece.js
+++ src/scripts/PuzzlePiece.js
@@ -9,6 +9,46 @@ export class PuzzlePiece {
 
         this.sprite.anchor.set(0.5);
         this.sprite.scale.set(0.5);
+
+        // adding interaction
+        this.field = field;
+        this.setInteractive();
+    }
+
+    setInteractive() {
+        this.sprite.interactive = true;
+        this.sprite.on("pointerdown", this.onTouchStart, this);
+        this.sprite.on("pointermove", this.onTouchMove, this);
+        this.sprite.on("pointerout", this.onTouchEnd, this);
+    }
+
+    onTouchStart(event) {
+        //save position of the cursor
+        this.touchPosition = {x: event.data.global.x, y: event.data.global.y};
+        this.dragging = true;
+    }
+
+    onTouchMove(event) {
+        //Assign the new postion for the sprite
+        if(!this.dragging) {
+            return; // do void if in dragging
+        }
+
+        // 1. Get the current position of the cursor
+        const currentPosition = {x: event.data.global.x, y: event.data.global.y};
+
+        // 2. Calculate the offset
+        const offsetX = currentPosition.x - this.touchPosition.x;
+        const offsetY = currentPosition.y - this.touchPosition.y;
+
+        // 3. Apply the offset to sprite
+        this.sprite.x = this.field.x + offsetX;
+        this.sprite.y = this.field.y + offsetY;
+
+    }
+
+    onTouchEnd() {
+        this.dragging = false;
     }
 
 }
